72a9d7f03bbd02362993c4dee2cc0751b553e095
Linyang He <leonlovesjazz@gmail.com>
2018-04-17 18:06:01 +0800

    Update

--- HW2/test.py
+++ HW2/test.py
@@ -1,19 +1,12 @@
-from mpl_toolkits.mplot3d import axes3d
+# from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
-from matplotlib import cm
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-X, Y, Z = axes3d.get_test_data(0.05)
-ax.plot_surface(X, Y, Z, rstride=8, cstride=8, alpha=0.3)
-cset = ax.contour(X, Y, Z, zdir='z', offset=-100, cmap=cm.coolwarm)
-cset = ax.contour(X, Y, Z, zdir='x', offset=-40, cmap=cm.coolwarm)
-cset = ax.contour(X, Y, Z, zdir='y', offset=40, cmap=cm.coolwarm)
-ax.set_xlabel('X')
-ax.set_xlim(-40, 40)
-ax.set_ylabel('Y')
-ax.set_ylim(-40, 40)
-ax.set_zlabel('Z')
-ax.set_zlim(-100, 100)
-ax.set_title("SHIT")
-plt.title("FUCK")
+# plt.show()
+
+fig, ax = plt.subplots(1, 2)
+ax[0].set_xlabel("test")
+ax[0].set_title("hashdfhasdhfh")
+ax[1].set_xlabel("test1")
+fig.head("asdfas")
+print(ax)
+plt.title("asdfasdfasd")
 plt.show()
