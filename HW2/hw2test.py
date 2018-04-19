f66e24005bd56c9b7f74126e396061a4ae08e8f9
Linyang He <leonlovesjazz@gmail.com>
2018-04-19 10:43:37 +0800

    Update

--- HW2/hw2test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
-fig, ax = plt.subplots(figsize=(9,6))
-ax.set_ylabel(r"Gdp's Growth Rate of the Country(%)")
-plt.xlabel("asdfasdf")
-ax.set_xlabel("in Year")
-plt.gca().set_xlabel("Test")
-plt.show()
